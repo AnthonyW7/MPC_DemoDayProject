516e48e0bd07dd446a0c7e16e37f56be85171650
anthonyASC6 <supershinytony@gmail.com>
2019-08-12 21:53:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//This code is only for demp presentations to switch divs before it is set up
+let presentSearch = document.getElementById("presentSearch");
+let search = document.getElementById("search");
+let home = document.getElementById("homePage");
+let presentHome = document.getElementById("presentHome");
+
+//making presentSearch switch from the home div to the search div
+presentSearch.onclick = function(event){
+  event.preventDefault();
+  home.style.display = "none";
+  search.style.display = "block";
+}
+
+//making presentHome switch from the search div to the home div
+presentHome.onclick = function(event){
+  event.preventDefault();
+  home.style.display = "block";
+  search.style.display = "none";
+}
